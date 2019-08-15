fcc203813e5f7f1e68b6027f54c209e4e247cf13
jmoore <josh@glencoesoftware.com>
2019-08-15 08:51:34 +0100

    Add static and templates dirs to MANIFEST.in

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ OmeroWeb is the container of the web clients for OMERO."
       url="https://github.com/openmicroscopy/openmicroscopy/",
       download_url="https://github.com/openmicroscopy/openmicroscopy/",
       packages=find_packages(exclude=("test",))+["omero.plugins"],
+      include_package_data=True,
       test_suite='test.suite',
       cmdclass={'test': PyTest},
       tests_require=['pytest<3'],
