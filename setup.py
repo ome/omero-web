52ab200ef9f56f53575f0020486528251f8ca230
jmoore <josh@glencoesoftware.com>
2019-08-27 17:22:15 +0200

    Activate travis deploy and remove PyTest

--- setup.py
+++ setup.py
@@ -10,29 +10,6 @@ import glob
 import sys
 import os
 
-import omero
-from omero_setup import PyTest
-
-for tools in glob.glob("../../../lib/repository/setuptools*.egg"):
-    if tools.find(".".join(map(str, sys.version_info[0:2]))) > 0:
-        sys.path.insert(0, os.path.abspath(tools))
-
-if "test" in sys.argv:
-    os.environ.setdefault('OMERO_HOME', os.path.abspath(
-        os.path.join("..", "..", "..", "dist")))
-
-    sys.path.insert(0, os.path.join("..", "target", "lib", "fallback"))
-    LIB = os.path.join("..", "target", "lib", "python")
-    sys.path.insert(0, LIB)
-    OMEROWEB_LIB = os.path.join(LIB, "omeroweb")
-    sys.path.insert(1, OMEROWEB_LIB)
-
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "omeroweb.settings")
-
-    import django
-    if django.VERSION > (1, 7):
-        django.setup()
-
 from setuptools import setup, find_packages
 
 setup(name="omero-web",
@@ -47,7 +24,5 @@ OmeroWeb is the container of the web clients for OMERO."
       download_url="https://github.com/openmicroscopy/openmicroscopy/",
       packages=find_packages(exclude=("test",))+["omero.plugins"],
       include_package_data=True,
-      test_suite='test.suite',
-      cmdclass={'test': PyTest},
       tests_require=['pytest<3'],
       )
