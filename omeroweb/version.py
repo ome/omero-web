4ebf0d1d115fd2bc88a7326d78c530f0c62d417a
jmoore <josh@glencoesoftware.com>
2019-08-30 09:06:08 +0100

    Add a default for omero-py versions

--- omeroweb/version.py
+++ omeroweb/version.py
@@ -1,5 +1,10 @@
-from omero_version import omero_version
-from omero_version import build_year as omero_buildyear
+try:
+    from omero_version import omero_version
+    from omero_version import build_year as omero_buildyear
+except ImportError as ie:
+    # Especially common during setup.py
+    omero_version = "unknown"
+    omero_buildyear = "unknown"
 
 
 omeroweb_version = "5.5.dev2"
