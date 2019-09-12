6d1fb1cbd1921c0043e9b650cc9784249c3ebab1
William Moore <w.moore@dundee.ac.uk>
2019-09-12 22:00:45 +0100

    octal literals specified with a leading '0o'

--- omeroweb/settings.py
+++ omeroweb/settings.py
@@ -1118,7 +1118,7 @@ except NameError:
             )
             with os.fdopen(os.open(secret_path,
                                    os.O_WRONLY | os.O_CREAT,
-                                   0600), 'w') as secret_file:
+                                   0o600), 'w') as secret_file:
                 secret_file.write(secret_key)
         except IOError as e:
             raise IOError("Please create a %s file with random characters"
