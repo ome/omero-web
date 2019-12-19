36c62f8717c822c1a3529ced399d363963e577c8
William Moore <w.moore@dundee.ac.uk>
2019-12-19 14:34:27 +0000

    Py3 only

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(name="omero-web",
           'or later (GPLv2+)',
           'Natural Language :: English',
           'Operating System :: OS Independent',
-          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
           'Topic :: Software Development :: Libraries :: Python Modules',
       ],  # Get strings from
           # http://pypi.python.org/pypi?%3Aaction=list_classifiers
