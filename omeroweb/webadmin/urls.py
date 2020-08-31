c182397290fe207bcc4b5f04198539f9d2e375e7
bramalingam <balaji.ramalingam.27@gmail.com>
2020-08-31 21:41:38 +0100

    replace re_path with path and update regex partially: webadmin

--- omeroweb/webadmin/urls.py
+++ omeroweb/webadmin/urls.py
@@ -23,43 +23,43 @@
 # Version: 1.0
 #
 
-from django.urls import re_path
+from django.urls import re_path, path
 
 from omeroweb.webadmin import views
 from omeroweb.webclient.views import WebclientLoginView
 
 # url patterns
 urlpatterns = [
-    re_path(r'^$', views.index, name="waindex"),
-    re_path(r'^login/$', WebclientLoginView.as_view(),
+    path(r'', views.index, name="waindex"),
+    path(r'login/', WebclientLoginView.as_view(),
         name="walogin"),
-    re_path(r'^logout/$', views.logout, name="walogout"),
-    re_path(r'^forgottenpassword/$', views.forgotten_password,
+    path(r'logout/', views.logout, name="walogout"),
+    path(r'forgottenpassword/', views.forgotten_password,
         name="waforgottenpassword"),
-    re_path(r'^experimenters/$', views.experimenters, name="waexperimenters"),
+    path(r'experimenters/', views.experimenters, name="waexperimenters"),
     re_path(r'^experimenter/(?P<action>[a-z]+)/(?:(?P<eid>[0-9]+)/)?$',
         views.manage_experimenter, name="wamanageexperimenterid"),
-    re_path(r'^change_password/(?P<eid>[0-9]+)/$', views.manage_password,
+    path(r'change_password/<int:eid>/', views.manage_password,
         name="wamanagechangepasswordid"),
-    re_path(r'^groups/$', views.groups, name="wagroups"),
+    path(r'groups/', views.groups, name="wagroups"),
     re_path(r'^group/(?P<action>((?i)new|create|edit|save))/'
         '(?:(?P<gid>[0-9]+)/)?$', views.manage_group, name="wamanagegroupid"),
     re_path(r'^group_owner/(?P<action>((?i)edit|save))/(?P<gid>[0-9]+)/$',
         views.manage_group_owner, name="wamanagegroupownerid"),
     re_path(r'^myaccount/(?:(?P<action>[a-z]+)/)?$',
         views.my_account, name="wamyaccount"),
-    re_path(r'^stats/$', views.stats, name="wastats"),
-    re_path(r'^drivespace_json/groups/$', views.drivespace_json,
+    path(r'stats/', views.stats, name="wastats"),
+    path(r'drivespace_json/groups/', views.drivespace_json,
         {'query': 'groups'}, name="waloaddrivespace_groups"),
-    re_path(r'^drivespace_json/users/$', views.drivespace_json,
+    path(r'drivespace_json/users/', views.drivespace_json,
         {'query': 'users'}, name="waloaddrivespace_users"),
-    re_path(r'^drivespace_json/group/(?P<groupId>[0-9]+)/$',
+    path(r'drivespace_json/group/<int:groupId>/',
         views.drivespace_json, name="waloaddrivespace_group"),
-    re_path(r'^drivespace_json/user/(?P<userId>[0-9]+)/$',
+    path(r'drivespace_json/user/<int:userId>/',
         views.drivespace_json, name="waloaddrivespace_user"),
 
     re_path(r'^change_avatar/(?P<eid>[0-9]+)/(?:(?P<action>[a-z]+)/)?$',
         views.manage_avatar, name="wamanageavatar"),
-    re_path(r'^myphoto/$', views.myphoto, name="wamyphoto"),
-    re_path(r'^email/$', views.email, name="waemail"),
+    path(r'myphoto/', views.myphoto, name="wamyphoto"),
+    path(r'email/', views.email, name="waemail"),
 ]
