cb54f1c08e5ac3608b8019d59bd294f07676e689
bramalingam <balaji.ramalingam.27@gmail.com>
2020-08-31 22:42:12 +0100

    replace re_path with path and update regex partially: webclient

--- omeroweb/webclient/urls.py
+++ omeroweb/webclient/urls.py
@@ -24,7 +24,7 @@
 #
 
 from django.conf import settings
-from django.urls import re_path
+from django.urls import re_path, path
 
 from omeroweb.webclient import views
 from omeroweb.webgateway import views as webgateway
@@ -36,33 +36,33 @@ viewer_view = get_callable(settings.VIEWER_VIEW)
 urlpatterns = [
 
     # Home page is the main 'Data' page
-    re_path(r'^$', views.load_template, {'menu': 'userdata'}, name="webindex"),
+    path(r'', views.load_template, {'menu': 'userdata'}, name="webindex"),
 
     # render main template
     re_path(r'^(?P<menu>((?i)userdata|public|history|search|help|usertags))/$',
         views.load_template,
         name="load_template"),
-    re_path(r'^userdata/$',
+    path(r'userdata/',
         views.load_template, {'menu': 'userdata'},
         name="userdata"),
-    re_path(r'^history/$',
+    path(r'history/',
         views.load_template, {'menu': 'history'},
         name="history"),
 
-    re_path(r'^login/$', views.WebclientLoginView.as_view(), name="weblogin"),
-    re_path(r'^logout/$', views.logout, name="weblogout"),
-    re_path(r'^active_group/$',
+    path(r'login/', views.WebclientLoginView.as_view(), name="weblogin"),
+    path(r'logout/', views.logout, name="weblogout"),
+    path(r'active_group/',
         views.change_active_group,
         name="change_active_group"),
 
     # The content of group/users drop-down menu
-    re_path(r'^group_user_content/$',
+    path(r'group_user_content/',
         views.group_user_content,
         name="group_user_content"),
 
     # update, display activities, E.g. delete queues, scripts etc.
-    re_path(r'^activities/', views.activities, name="activities"),
-    re_path(r'^activities_json/',
+    path(r'activities/', views.activities, name="activities"),
+    path(r'activities_json/',
         views.activities,
         {'template': 'json'},
         name="activities_json"),
@@ -79,7 +79,7 @@ urlpatterns = [
 
     # chgrp. Load potential target groups, then load target P/D within chosen
     # group
-    re_path(r'^load_chgrp_groups/$',
+    path(r'load_chgrp_groups/',
         views.load_chgrp_groups,
         name="load_chgrp_groups"),  # Query E.g. ?Image=1,2&Dataset=3
     re_path(r'^load_chgrp_target/(?P<group_id>[0-9]+)/'
@@ -133,7 +133,7 @@ urlpatterns = [
         webgateway.render_thumbnail,
         {'_defcb': defaultThumbnail},
         name="render_thumbnail_resize"),
-    re_path(r'^edit_channel_names/(?P<imageId>[0-9]+)/$',
+    path(r'edit_channel_names/<int:imageId>/',
         views.edit_channel_names,
         name="edit_channel_names"),
 
@@ -195,60 +195,60 @@ urlpatterns = [
 
     # chgrp dry run - 'group_id', obj-types and ids in POST data.
     # E.g. Dataset=1,2,3 & Fileset=4. Multiple datatypes in one chgrp.
-    re_path(r'^chgrpDryRun/$', views.chgrpDryRun, name="chgrpDryRun"),
+    path(r'chgrpDryRun/', views.chgrpDryRun, name="chgrpDryRun"),
 
     # Popup for downloading original archived files for images
-    re_path(r'^download_placeholder/$', views.download_placeholder,
+    path(r'download_placeholder/', views.download_placeholder,
         name="download_placeholder"),
 
     # chgrp - 'group_id', obj-types and ids in POST data
-    re_path(r'^chgrp/$', views.chgrp, name="chgrp"),
+    path(r'chgrp/', views.chgrp, name="chgrp"),
 
     # annotations
     re_path(r'^action/(?P<action>[a-zA-Z]+)/(?:(?P<o_type>[a-zA-Z]+)/)'
         r'?(?:(?P<o_id>[0-9]+)/)?$',
         views.manage_action_containers,
         name="manage_action_containers"),
-    re_path(r'^batch_annotate/$', views.batch_annotate, name="batch_annotate"),
-    re_path(r'^annotate_tags/$', views.annotate_tags, name="annotate_tags"),
-    re_path(r'^marshal_tagging_form_data/$', views.marshal_tagging_form_data,
+    path(r'batch_annotate/', views.batch_annotate, name="batch_annotate"),
+    path(r'annotate_tags/', views.annotate_tags, name="annotate_tags"),
+    path(r'marshal_tagging_form_data/', views.marshal_tagging_form_data,
         name="marshal_tagging_form_data"),
-    re_path(r'^annotate_rating/$',
+    path(r'annotate_rating/',
         views.annotate_rating,
         name="annotate_rating"),
-    re_path(r'^annotate_comment/$',
+    path(r'^annotate_comment/',
         views.annotate_comment,
         name="annotate_comment"),
-    re_path(r'^annotate_file/$', views.annotate_file, name="annotate_file"),
-    re_path(r'^annotate_map/$', views.annotate_map, name="annotate_map"),
-    re_path(r'^annotation/(?P<annId>[0-9]+)/$',
+    path(r'annotate_file/', views.annotate_file, name="annotate_file"),
+    path(r'annotate_map/', views.annotate_map, name="annotate_map"),
+    path(r'annotation/<int:annId>/',
         views.download_annotation,
         name="download_annotation"),
     re_path(r'^load_original_metadata/(?P<imageId>[0-9]+)/'
         r'(?:(?P<share_id>[0-9]+)/)?$',
         views.load_original_metadata,
         name="load_original_metadata"),
-    re_path(r'^download_orig_metadata/(?P<imageId>[0-9]+)/$',
+    path(r'download_orig_metadata/<int:imageId>/',
         views.download_orig_metadata,
         name="download_orig_metadata"),
     re_path(r'^omero_table/(?P<file_id>[0-9]+)/(?:(?P<mtype>((?i)json|csv))/)?$',
         views.omero_table,
         name="omero_table"),
 
-    re_path(r'^avatar/(?P<oid>[0-9]+)/$', views.avatar, name="avatar"),
+    path(r'^avatar/<int:oid>/', views.avatar, name="avatar"),
 
 
     # scripting service urls
-    re_path(r'^list_scripts/$',
+    path(r'list_scripts/',
         views.list_scripts,
         name="list_scripts"),  # returns html list of scripts - click to run
-    re_path(r'^script_ui/(?P<scriptId>[0-9]+)/$',
+    path(r'script_ui/<int:scriptId>/',
         views.script_ui,
         name='script_ui'),  # shows a form for running a script
-    re_path(r'^script_run/(?P<scriptId>[0-9]+)/$',
+    path(r'script_run/<int:scriptId>/',
         views.script_run,
         name='script_run'),  # runs the script - parameters in POST
-    re_path(r'^script_upload/$', views.script_upload, name='script_upload'),
+    path(r'script_upload/', views.script_upload, name='script_upload'),
     re_path(r'^get_original_file/(?:(?P<fileId>[0-9]+)/)?$',
         views.get_original_file,
         name="get_original_file"),  # for stderr, stdout etc
@@ -263,19 +263,19 @@ urlpatterns = [
 
     # ome_tiff_script: generate OME-TIFF and attach to image (use script
     # service). Must be POST
-    re_path(r'^ome_tiff_script/(?P<imageId>[0-9]+)/$',
+    path(r'^ome_tiff_script/<int:imageId>/',
         views.ome_tiff_script,
         name='ome_tiff_script'),
-    re_path(r'^ome_tiff_info/(?P<imageId>[0-9]+)/$',
+    path(r'^ome_tiff_info/<int:imageId>/',
         views.ome_tiff_info,
         name='ome_tiff_info'),
 
     # ping OMERO server to keep session alive
-    re_path(r'^keepalive_ping/$', views.keepalive_ping, name="keepalive_ping"),
+    path(r'keepalive_ping/', views.keepalive_ping, name="keepalive_ping"),
 
     # Load data, but with JSON.
     # re_path(r'^api/$', None, name='api'),
-    re_path(r'^api/groups/$', views.api_group_list,
+    path(r'api/groups/', views.api_group_list,
         name='api_groups'),
 
     re_path(r'^api/experimenters/(?P<experimenter_id>-?\d+)/$',
@@ -283,49 +283,49 @@ urlpatterns = [
 
     # Generic container list. This is necessary as an experimenter may have
     # datasets/etc which do not belong to any project
-    re_path(r'^api/containers/$', views.api_container_list, name='api_containers'),
+    path(r'api/containers/', views.api_container_list, name='api_containers'),
 
     # re_path(r'^api/projects/$', views.api_project_list, name='api_projects'),
     # re_path(r'^api/projects/(?P<pk>[0-9]+)/$', views.api_project_detail),
 
-    re_path(r'^api/datasets/$', views.api_dataset_list, name='api_datasets'),
+    path(r'api/datasets/', views.api_dataset_list, name='api_datasets'),
     # re_path(r'^api/datasets/(?P<pk>[0-9]+)/$', views.api_dataset_detail),
 
-    re_path(r'^api/images/$', views.api_image_list, name='api_images'),
+    path(r'api/images/', views.api_image_list, name='api_images'),
 
     # special case: share_id not allowed in query string since we
     # just want to allow share connection for this url ONLY.
-    re_path(r'^api/share_images/(?P<share_id>[0-9]+)/$', views.api_image_list,
+    path(r'api/share_images/<int:share_id>/', views.api_image_list,
         name='api_share_images'),
 
-    re_path(r'^api/plates/$', views.api_plate_list, name='api_plates'),
+    path(r'api/plates/', views.api_plate_list, name='api_plates'),
     # re_path(r'^api/plates/(?P<pk>[0-9]+)/$', views.api_plate_detail),
 
-    re_path(r'^api/plate_acquisitions/$', views.api_plate_acquisition_list,
+    path(r'api/plate_acquisitions/', views.api_plate_acquisition_list,
         name='api_plate_acquisitions'),
     # re_path(r'^api/plate_acquisitions/(?P<pk>[0-9]+)/$',
     #     views.api_plate_acquisitions_detail),
 
     # POST to create link, DELETE to remove.
     # links in request.body json, e.g. {"dataset":{"10":{"image":[1,2,3]}}}
-    re_path(r'^api/links/$', views.api_links, name='api_links'),
+    path(r'api/links/', views.api_links, name='api_links'),
 
     # re_path(r'^api/tags/$', views.api_tag_list, name='api_tags'),
     # re_path(r'^api/tags/(?P<pk>[0-9]+)/$', views.api_tag_detail),
 
     # Retrieve paths to an object
-    re_path(r'^api/paths_to_object/$', views.api_paths_to_object,
+    path(r'api/paths_to_object/', views.api_paths_to_object,
         name='api_paths_to_object'),
 
     # Get parents of 1 or more objects. ?image=1,2&dataset=3
-    re_path(r'^api/parent_links/$', views.api_parent_links,
+    path(r'api/parent_links/', views.api_parent_links,
         name='api_parent_links'),
 
-    re_path(r'^api/tags/$', views.api_tags_and_tagged_list,
+    path(r'api/tags/', views.api_tags_and_tagged_list,
         name='api_tags_and_tagged'),
 
-    re_path(r'^api/annotations/$', views.api_annotations,
+    path(r'api/annotations/', views.api_annotations,
         name='api_annotations'),
 
-    re_path(r'^api/shares/$', views.api_share_list, name='api_shares'),
+    path(r'api/shares/', views.api_share_list, name='api_shares'),
 ]
