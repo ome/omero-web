bf092046c5cec623c029f7a687d2cc2c2055302b
Emil Rozbicki <emil@glencoesoftware.com>
2021-08-25 13:07:29 +0200

    Remove quotes

--- omeroweb/settings.py
+++ omeroweb/settings.py
@@ -832,7 +832,7 @@ CUSTOM_SETTINGS_MAPPINGS = {
     ],
     "omero.web.favicon_url": [
         "FAVICON_URL",
-        '"webgateway/img/ome.ico"',
+        "webgateway/img/ome.ico",
         str,
         ("Favicon URL, specifies the path relative to django's static file dirs."),
     ],
