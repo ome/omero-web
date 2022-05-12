3c35e0fa18611979682fcf54efdece585998af2c
William Moore <w.moore@dundee.ac.uk>
2022-05-12 10:44:27 +0100

    Use search form html and js from omero_search_engine_client

--- omeroweb/webclient/views.py
+++ omeroweb/webclient/views.py
@@ -141,11 +141,11 @@ logger.info("INIT '%s'" % os.getpid())
 # all the annotations expected for a PAGE of images
 ANNOTATIONS_LIMIT = settings.PAGE * 100
 
-ELASTICSEARCH_URL = None
+SEARCH_ENGINE_URL = None
 if settings.ELASTICSEARCH_ENABLED:
     try:
         from omero_search_engine_client import search_engine_settings
-        ELASTICSEARCH_URL = search_engine_settings.SEARCH_ENGINE_URL
+        SEARCH_ENGINE_URL = search_engine_settings.SEARCH_ENGINE_URL
     except ImportError:
         logger.debug("Failed to import omero_search_engine_client settings")
 
@@ -491,7 +491,7 @@ def _load_template(request, menu, conn=None, url=None, **kwargs):
     if menu == "search":
         if global_search_form.is_valid():
             init["query"] = global_search_form.cleaned_data["search_query"]
-        if menu == "search" and ELASTICSEARCH_URL is not None:
+        if menu == "search" and SEARCH_ENGINE_URL is not None:
             if not init["query"].isnumeric():
                 # for IDs, don't use elastic
                 template = "webclient/search/elasticsearch.html"
@@ -578,7 +578,7 @@ def _load_template(request, menu, conn=None, url=None, **kwargs):
     context["member_of_groups"] = conn.getEventContext().memberOfGroups
     context["search_default_user"] = settings.SEARCH_DEFAULT_USER
     context["search_default_group"] = settings.SEARCH_DEFAULT_GROUP
-    context["elasticsearch_url"] = ELASTICSEARCH_URL
+    context["search_engine_url"] = SEARCH_ENGINE_URL
 
     return context
 
