fb165ae368f902b54cfc5a4f1b0df6339673565e
Andreas Knab <andreas@glencoesoftware.com>
2024-03-18 13:42:08 -0400

    Print timing to console

--- omeroweb/webclient/static/webclient/javascript/ome.progress_overlay.js
+++ omeroweb/webclient/static/webclient/javascript/ome.progress_overlay.js
@@ -6,10 +6,12 @@
  * or repeated before it is completed.
  * @param promise When this promise resolves (successfully or otherwise) the overlay closes
  * @param message The optional message to display (defaults to "Please wait")
+ * @param quiet Don't print timing to console
  * @returns {*|jQuery} The jQuery element holding the dialog; no need to do anything with it
  */
-OME.progress_overlay = function (promise, message) {
+OME.progress_overlay = function (promise, message, quiet) {
   'use strict';
+  const startTime = new Date().getTime();
   const dialog = $('<div>' + (message || 'Please wait') + '</div>')
     .appendTo(document.body)
     .dialog({
@@ -22,7 +24,12 @@ OME.progress_overlay = function (promise, message) {
         'ui-dialog': 'ome-modal-progress',
       }
     });
-  promise.finally(() => dialog.dialog('destroy').remove());
+  promise.finally(() => {
+    dialog.dialog('destroy').remove();
+    if (!quiet) {
+      window.console.log('UI blocked for ' + (new Date().getTime() - startTime).toString() + 'ms');
+    }
+  });
   return dialog;
 };
 
