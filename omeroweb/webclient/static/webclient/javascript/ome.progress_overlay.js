3e68b74a9d60bb05a3aa17281f66c6ec41661d34
Andreas Knab <andreas@glencoesoftware.com>
2024-04-12 13:09:22 +0200

    Add spinner at overlay level

--- omeroweb/webclient/static/webclient/javascript/ome.progress_overlay.js
+++ omeroweb/webclient/static/webclient/javascript/ome.progress_overlay.js
@@ -12,7 +12,7 @@
 OME.progress_overlay = function (promise, message, quiet) {
   'use strict';
   const startTime = new Date().getTime();
-  const dialog = $('<div>' + (message || 'Please wait') + '</div>')
+  const dialog = $('<div><span class="spinner"></span>&nbsp;' + (message || 'Please wait') + '</div>')
     .appendTo(document.body)
     .dialog({
       modal: true,
@@ -47,6 +47,13 @@ OME.progress_overlay = function (promise, message, quiet) {
         align-items: center;
         font-size: larger;
         display: flex;
+    }
+    
+    .ome-modal-progress .spinner {
+      background-image: url("data:image/gif;base64,R0lGODlhEAAQAPIAAP///zxKW9HU2G95hjxKW4eQmqCnr6yyuSH5BAkKAAAAIf4aQ3JlYXRlZCB3aXRoIGFqYXhsb2FkLmluZm8AIf8LTkVUU0NBUEUyLjADAQAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOw==");
+      width: 16px;
+      height: 16px;
+      display: inline-block;
     }
   `;
   document.head.appendChild(styleSheet);
