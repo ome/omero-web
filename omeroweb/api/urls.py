54286f44fb13954e8e6d0f0dc3c069d4776756bf
William Moore <w.moore@dundee.ac.uk>
2026-08-20 14:35:06 +0100

    flake8 fix

--- omeroweb/api/urls.py
+++ omeroweb/api/urls.py
@@ -410,8 +410,9 @@ GET Annotations, using omero-marshal to generate json
 """
 
 api_namedannotations = re_path(
-    r"^v(?P<api_version>%s)/m/(?P<ann_type>file|map|tag|long|timestamp|comment|boolean|double|xml|term)annotations/$"
-    % versions,
+    r"^v(?P<api_version>%s)/m/"
+    r"(?P<ann_type>file|map|tag|long|timestamp|comment|boolean|double|xml|term)"
+    r"annotations/$" % versions,
     views.AnnotationsView.as_view(),
     name="api_namedannotations",
 )
